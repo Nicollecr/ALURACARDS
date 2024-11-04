0575db1b7df1f7faa43aed2735f3f92d22d5ad6f
Nicollecr <00001102979478sp@al.educacao.sp.gov.br>
2024-11-04 11:57:46 -0300

    Minhas atividades

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Mundo da música',
+    'Qual o maior grupo sul-coreano do mundo?',
+    'O BTS, um grupo sul-coreano lançando em 2013 pela empresa Big Hit com 7 membros'
+)
+
+criaCartao(
+    'Culinaria',
+    'Qual é a comida mais famosa do Brasil?',
+    'É a feijoada'
+)
+
+criaCartao(
+    'Mundo Pop',
+    'Quem é o rei do pop?',
+    'Michael Jackson'
+)
+
+criaCartao(
+    'Anime',
+    'Qual é o pai de Monkey.D.Luffy?',
+    'Monkey.D.Dragon'
+)
